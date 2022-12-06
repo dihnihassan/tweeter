a6254e75692e021b670b186dd8839de30038950f
Dihni Hassan <dihnihassan@gmail.com>
2022-12-06 18:29:36 -0500

    Add character and error in newtweet.css and composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+// console.log("Is this working!");
+
+$(document).ready(function() {
+  $("#tweet-text").on('input', function () {
+    const countNumber = 140;
+    let newCount = countNumber - $(this).val().length;
+    // $(this).parent().siblings('.tweet-footer').children('.counter').val(newCount);
+    let tweetCount = $(this).parent().children('.tweet-footer').children('.counter');
+
+     tweetCount.val(newCount); 
+
+    if (newCount < 0) {
+      tweetCount.addClass("tweet-error");
+    } else {
+      tweetCount.removeClass("tweet-error");
+    }
+
+  });
+});
+  
