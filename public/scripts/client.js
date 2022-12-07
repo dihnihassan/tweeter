63bc13aa2771ccf31e31908b09b221e38bdbacc8
Dihni Hassan <dihnihassan@gmail.com>
2022-12-07 14:22:27 -0500

    Add dynamic tweets use functions in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,94 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+
+  };
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+
+$(document).ready(function() {
+  const createTweetElement = function(tweetData) {
+    console.log("Inside createtweetelement", tweetData);
+
+    const $tweet = $(`
+    <article class="tweet">
+    <header>
+      <div>
+        <img src="${tweetData.user.avatars}" alt="avatar">
+        <p>${tweetData.user.name}</p>
+      </div>
+      <p>${tweetData.user.handle}</p>
+    </header>
+    <p>${tweetData.content.text}</p>
+  <hr/>
+    <footer>
+      <p>${tweetData.created_at}</p>
+      <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+    `);
+    
+    return $tweet;
+    
+  }
+
+  const renderTweets = function(tweets) {
+    console.log("This is for tweets", tweets);
+    for(const item of tweets) {
+      console.log("This is for items", item);
+      const $tweet = createTweetElement(item);
+      $('#tweets-container').append($tweet);
+
+    }
+    
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  }
+
+  renderTweets(data);
+
+  
+  // console.log($tweet); 
+  
+});
