527c46fe1c3c76878c1e34d7162ce4087aa9f2fd
Dihni Hassan <dihnihassan@gmail.com>
2022-12-07 17:45:34 -0500

    Add timeago function to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,45 +6,6 @@
 
 
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-
-};
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 
 $(document).ready(function () {
   const createTweetElement = function (tweetData) {
@@ -62,7 +23,7 @@ $(document).ready(function () {
     <p>${tweetData.content.text}</p>
   <hr/>
     <footer>
-      <p>${tweetData.created_at}</p>
+      <p>${timeago.format(tweetData.created_at)}</p>
       <div>
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -78,10 +39,13 @@ $(document).ready(function () {
 
   const renderTweets = function (tweets) {
     // console.log("This is for tweets", tweets);
+
+    $('#tweets-container').empty();
+
     for (const item of tweets) {
       // console.log("This is for items", item);
       const $tweet = createTweetElement(item);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
 
     }
 
